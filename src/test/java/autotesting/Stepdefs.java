edbc851625ca9891e942c57bb8e0094b2990c058
StefanBotman <stefanbotman@gmail.com>
2018-07-17 13:16:06 +0200

    update

--- src/test/java/autotesting/Stepdefs.java
+++ src/test/java/autotesting/Stepdefs.java
@@ -27,7 +27,7 @@ public class Stepdefs {
 
 	@Given("^username \"([^\"]*)\" and password \"([^\"]*)\"$")
 	public void username_and_password(String arg1, String arg2) throws Exception {
-		// Write code here that turns the phrase above into concrete actions - test5
+		// Write code here that turns the phrase above into concrete actions - test6
 		throw new PendingException();
 	}
 
